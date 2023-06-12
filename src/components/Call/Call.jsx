a71c30c40862b5a66276bc8341c35fcabd1a6a32
unknown <pitakxdd@gmail.com>
2023-06-12 23:04:01 +0300

    Created hovered button from Call component

--- src/components/Call/Call.jsx
+++ src/components/Call/Call.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import incoming from '../../images/incoming.svg'
 import outcoming from '../../images/outcoming.svg'
+import select from '../../images/select.svg'
 import formatPhoneNumber from '../../utils/formatPhoneNumber';
 import formatTimeDuration from '../../utils/formatTimeDuration';
 import formatTimeDate from '../../utils/formatTimeDate'
 
 const Call = ({ data }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <td className="call">
-      {data.in_out === 1 ?
-        (<img src={incoming} className="call__type" alt='Иконка типа звонка'></img>)
-        :
-        (<img src={outcoming} className="call__type" alt='Иконка типа звонка'></img>)}
+    <td className="call" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {isHovered && (
+        <button className="call__select-button" style={{ backgroundImage: `url(${select})` }} />
+      )}
+
+      {data.in_out === 1 ? (
+        <img src={incoming} className="call__type" alt='Иконка типа звонка' />
+      ) : (
+        <img src={outcoming} className="call__type" alt='Иконка типа звонка' />
+      )}
 
       <p className="call__time">{formatTimeDate(data.time)}</p>
       <img src={data.person_avatar} alt="Аватар сотрудника" className="call__employee" />
--- src/components/FilterSearch/FilterSearch.jsx
+++ src/components/FilterSearch/FilterSearch.jsx
@@ -4,15 +4,23 @@ import ArrowIcon from '../ArrowIcon/ArrowIcon';
 
 const OPTIONS = {
   types: ['Все типы', 'Входящие', 'Исходящие'],
-  employees: ['Все сотрудники', 'Константин', 'Полина'],
-  calls: ['Все клиенты', 'Новые клиенты', 'Все исполнители', 'Через приложение', 'Прочие звонки'],
-  errors: ['Все ошибки', 'Приветствие', 'Имя', 'Цена', 'Скидка', 'Предзаказ', 'Благодарность', 'Стоп слова'],
+  employees: ['Все сотрудники', 'Константин К.', 'Полина Ю.'],
+  calls: ['Все звонки', 'Новые клиенты', 'Все исполнители', 'Через приложение', 'Прочие звонки'],
   ratings: ['Все оценки', 'Распознать', 'Скрипт не использован', 'Плохо', 'Хорошо', 'Отлично'],
+  errors: ['Все ошибки', 'Приветствие', 'Имя', 'Цена', 'Скидка', 'Предзаказ', 'Благодарность', 'Стоп слова'],
 };
 
 const FilterSearch = () => {
   const [isOpen, setIsOpen] = useState({});
-  const [selectedOptions, setSelectedOptions] = useState({});
+  const [selectedOptions, setSelectedOptions] = useState({
+    types: 'Все типы',
+    employees: 'Все сотрудники',
+    calls: 'Все звонки',
+    ratings: 'Все оценки',
+    errors: 'Все ошибки',
+  });
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [selectedOption, setSelectedOption] = useState({});
 
   const makeToggleMenu = (key) => () => {
     setIsOpen((prevState) => ({
@@ -22,28 +30,50 @@ const FilterSearch = () => {
   };
 
   const handleOptionSelect = (option, key) => {
+    setSelectedOption((prevSelectedOption) => ({
+      ...prevSelectedOption,
+      [key]: option,
+    }));
+
     setSelectedOptions((prevOptions) => ({
       ...prevOptions,
       [key]: option,
     }));
+
+    const updatedOptions = [option, ...OPTIONS[key].filter((item) => item !== option)];
+    OPTIONS[key] = updatedOptions;
     makeToggleMenu(key)();
+    setIsFiltered(true);
   };
 
-  const renderDropdownOptions = (key) => {
-    return (
-      <ul className="panel__dropdown">
-        {OPTIONS[key].map((option) => (
-          <li
-            key={option}
-            className="panel__drop-element"
-            onClick={() => handleOptionSelect(option, key)}
-          >
-            {option}
-          </li>
-        ))}
-      </ul>
-    );
+  const resetFilters = () => {
+    setSelectedOptions({
+      types: 'Все типы',
+      employees: 'Все сотрудники',
+      calls: 'Все звонки',
+      ratings: 'Все оценки',
+      errors: 'Все ошибки',
+    });
+    setIsFiltered(false);
+    setSelectedOption({});
   };
+
+  const renderDropdownOptions = (key) => (
+    <ul className="panel__dropdown">
+      {OPTIONS[key].map((option) => (
+        <li
+          key={option}
+          className={`panel__drop-element ${selectedOption[key] === option ? 'selected' : ''}`}
+          onClick={() => {
+            handleOptionSelect(option, key);
+            makeToggleMenu(key)();
+          }}
+        >
+          {option}
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
     <div className="panel">
@@ -55,15 +85,24 @@ const FilterSearch = () => {
       </div>
 
       <div className="panel__container-right">
-        {Object.keys(OPTIONS).map((optionKey) => (
+        {isFiltered && (
+          <button className="panel__button reset-button" onClick={resetFilters}>
+            <span className="panel__text">Сбросить фильтры</span>
+            <svg width="9" height="9" viewBox="0 0 9 9" fill="#ADBFDF" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8.75 0.88125L7.86875 0L4.375 3.49375L0.88125 0L0 0.88125L3.49375 4.375L0 7.86875L0.88125 8.75L4.375 5.25625L7.86875 8.75L8.75 7.86875L5.25625 4.375L8.75 0.88125Z" />
+            </svg>
+          </button>
+        )}
+
+        {Object.keys(selectedOptions).map((optionKey) => (
           <button
+            className={`panel__button ${selectedOption[optionKey] ? 'selected' : ''}`}
             key={optionKey}
-            className="panel__button"
             onClick={makeToggleMenu(optionKey)}
           >
-            <p className="panel__text">
-              {selectedOptions[optionKey] || OPTIONS[optionKey][0]}
-            </p>
+            <span className={`panel__text ${selectedOption[optionKey] ? 'selected' : ''}`}>
+              {selectedOptions[optionKey]}
+            </span>
             <ArrowIcon className={isOpen[optionKey] ? 'panel__arrow rotate' : 'panel__arrow'} />
             {isOpen[optionKey] && renderDropdownOptions(optionKey)}
           </button>
