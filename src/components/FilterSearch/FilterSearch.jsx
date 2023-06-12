2b7d74bc30386567c8db029af431dc5bd2a2772d
unknown <pitakxdd@gmail.com>
2023-06-12 17:42:44 +0300

    Created ArrowIcon component

--- src/components/FilterSearch/FilterSearch.jsx
+++ src/components/FilterSearch/FilterSearch.jsx
@@ -1,38 +1,65 @@
-import React from "react";
+import React, { useState } from 'react';
 import search from '../../images/search.svg'
-import arrow from '../../images/arrow.svg'
+
+import ArrowIcon from '../ArrowIcon/ArrowIcon'
 
 const FilterSearch = () => {
+
+  const [menuStates, setMenuStates] = useState({
+    types: false,
+    employees: false,
+    calls: false,
+    ratings: false,
+    errors: false,
+  });
+
+  const toggleMenu = (key) => {
+    setMenuStates((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
+  };
+
   return (
     <div className="panel">
       <div className="panel__container-left">
         <button className="panel__button-search">
-          <img src={search}></img>
+          <img src={search} alt='Кнопка поиска'></img>
         </button>
         <p className="panel__text">Поиск по звонкам</p>
       </div>
+
       <div className="panel__container-right">
-        <p className="panel__text">Все типы</p>
-        <button className="panel__button">
-          <img src={arrow} alt='кнопка развернуть' />
+        <button className="panel__button" onClick={() => toggleMenu("types")}>
+          <p className="panel__text">Все типы</p>
+          <ArrowIcon className={menuStates["types"] ? "panel__arrow rotate" : "panel__arrow"} />
         </button>
-        <p className="panel__text">Все сотрудники</p>
-        <button className="panel__button">
-          <img src={arrow} alt='кнопка развернуть' />
+
+        <button className="panel__button" onClick={() => toggleMenu("employees")}>
+          <p className="panel__text">Все сотрудники</p>
+          <ArrowIcon className={menuStates["employees"] ? "panel__arrow rotate" : "panel__arrow"} />
         </button>
-        <p className="panel__text">Все звонки</p>
-        <button className="panel__button">
-          <img src={arrow} alt='кнопка развернуть' />
+
+        <button className="panel__button" onClick={() => toggleMenu("calls")}>
+          <p className="panel__text">Все звонки</p>
+          <ArrowIcon className={menuStates["calls"] ? "panel__arrow rotate" : "panel__arrow"} />
         </button>
-        <p className="panel__text">Все оценки</p>
-        <button className="panel__button">  <img src={arrow} alt='кнопка развернуть' /></button>
-        <p className="panel__text">Все ошибки</p>
-        <button className="panel__button">
-          <img src={arrow} alt='кнопка развернуть' />
+
+        <button className="panel__button" onClick={() => toggleMenu("ratings")}>
+          <p className="panel__text">Все оценки</p>
+          <ArrowIcon className={menuStates["ratings"] ? "panel__arrow rotate" : "panel__arrow"} />
         </button>
+
+        <button className="panel__button" onClick={() => toggleMenu("errors")}>
+          <p className="panel__text">Все ошибки</p>
+          <ArrowIcon className={menuStates["errors"] ? "panel__arrow rotate" : "panel__arrow"} />
+        </button>
+
       </div>
-    </div>
+    </div >
   )
 }
 
 export default FilterSearch;
+
+
